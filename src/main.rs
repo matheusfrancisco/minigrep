fac8ac01c942ad5756a3f9dc6323ecdaef72d975
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-07-21 11:53:19 -0300

    Add: init minigrep

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, fs, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    println!("{:?}", args);
+    let contents = fs::read_to_string(config.filename).expect("Error parsing the file");
+    println!("With text  {}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
